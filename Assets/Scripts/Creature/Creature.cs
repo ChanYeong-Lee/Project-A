103e3cd05aaa028ed1eece2677a0c5b1acd51cbe
User <kaseupeo01@gmail.com>
2024-02-26 21:51:52 +0900

    .

--- Assets/Scripts/Creature/Creature.cs
+++ Assets/Scripts/Creature/Creature.cs
@@ -11,12 +11,14 @@ public abstract class Creature : MonoBehaviour
     
     protected Stat currentStat;
     
+    protected Animator anim;
     protected StateMachine stateMachine;
     
     public CreatureData CreatureData => creatureData;
     public Vector3 SpawnPos { get => spawnPos; set => spawnPos = value; }
     public int CurrentLevel { get => currentLevel; set => currentLevel = value; }
     public Stat CurrentStat { get => currentStat; set => currentStat = value; }
+    public Animator Anim => anim;
 
     private void Start()
     {
@@ -26,6 +28,7 @@ public abstract class Creature : MonoBehaviour
     public virtual void Init()
     {
         stateMachine = gameObject.GetOrAddComponent<StateMachine>();
+        anim = GetComponent<Animator>();
 
         if (currentLevel < creatureData.Stats[0].Level || currentLevel > creatureData.Stats[^1].Level) 
             currentLevel = 1;
@@ -38,6 +41,7 @@ public abstract class Creature : MonoBehaviour
     protected abstract class CreatureState : BaseState
     {
         protected Creature owner;
+        protected Animator anim => owner.Anim;
         protected Vector3 spawnPos => owner.SpawnPos;
         
         public CreatureState(Creature owner)
