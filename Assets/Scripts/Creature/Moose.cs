064465c7da525ed2e8a6d6286a531cc9af9d5c44
User <kaseupeo01@gmail.com>
2024-02-27 16:21:30 +0900

    .

--- Assets/Scripts/Creature/Moose.cs
+++ Assets/Scripts/Creature/Moose.cs
@@ -7,11 +7,6 @@ public class Moose : Monster
 {
     [SerializeField] public State state;
     [SerializeField] public float time;
-    
-    private void Update()
-    {
-        time = anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
-    }
 
     public enum State
     {
@@ -36,10 +31,10 @@ public class Moose : Monster
     private class MooseState : MonsterState
     {
         protected Moose moose => owner as Moose;
+        protected bool isChangedState;
         protected float randTime;
         protected float vertical;
         protected float horizontal;
-        protected bool isStand;
         protected int idInt;
         
         public MooseState(Creature owner) : base(owner) { }
@@ -47,18 +42,19 @@ public class Moose : Monster
         public override void Update()
         {
             randTime -= Time.deltaTime;
-            if (randTime < 0) 
-                RandVariable();
+            
+            // 인스펙터 확인용 코드
+            moose.time = randTime;
         }
 
         // 랜덤 값 생성
-        // TODO : 패트롤일 때 달리기보다 옆으로 돌기만 해서 수치 조정 필요
-        protected void RandVariable()
+        // 랜덤 수치 값 일부 조정 가능한 함수(최소 시간, 최대 시간, 상태 머신 바뀔 확률)
+        protected void RandVariable(float minTime = 1f, float maxTime = 10f, float rateToChange = 0.5f)
         {
-            randTime = Random.Range(1f, 10f);
-            isStand = Random.value > 0.5f;
-            vertical = Random.Range(-1f, 3f);
-            horizontal = Random.Range(-2f, 2f);
+            randTime = Random.Range(minTime, maxTime);
+            isChangedState = Random.value < rateToChange;
+            vertical = Random.Range(-1f, 5f);
+            horizontal = Random.Range(-2f, 2f) * Random.value;
             idInt = Random.Range(-1, 11);
         }
     }
@@ -69,21 +65,28 @@ public class Moose : Monster
 
         public override void Enter()
         {
-            anim.SetBool("Stand", true);
-            
+            // 인스펙터 확인용 코드
             moose.state = State.Idle;
+
+            RandVariable(1f, 5f, 0);
+            anim.SetBool("Stand", true);
+            Debug.Log("IDLE");
         }
         
         public override void Update()
         {
             base.Update();
-            
+
             anim.SetInteger("IDInt", idInt);
+            
+            
+            if (randTime < 0) 
+                RandVariable(1f,2f, 0.9f);
         }
 
         public override void Transition()
         {
-            if (isStand && anim.IsInTransition(0)) 
+            if (isChangedState) 
                 ChangeState(State.Patrol);
         }
 
@@ -99,11 +102,12 @@ public class Moose : Monster
 
         public override void Enter()
         {
-            RandVariable();
-            
-            anim.SetBool("Stand", false);
-            
+            // 인스펙터 확인용 코드
             moose.state = State.Patrol;
+            
+            RandVariable(1f, 5f, 0);
+
+            anim.SetBool("Stand", false);
         }
 
         public override void Update()
@@ -112,15 +116,15 @@ public class Moose : Monster
             
             anim.SetFloat("Vertical", Mathf.Lerp(anim.GetFloat("Vertical"), vertical, Time.deltaTime));
             anim.SetFloat("Horizontal", Mathf.Lerp(anim.GetFloat("Horizontal"), horizontal, Time.deltaTime));
+            
+            if (randTime < 0) 
+                RandVariable(1f,2f, 0.3f);
         }
 
         public override void Transition()
         {
-            if (isStand)
-            {
+            if (isChangedState) 
                 ChangeState(State.Idle);
-                Debug.Log("stand");
-            }
         }
     }
     
@@ -130,7 +134,6 @@ public class Moose : Monster
             
         public override void Transition()
         {
-            
         }
     }
     
@@ -140,11 +143,8 @@ public class Moose : Monster
             
         public override void Transition()
         {
-            
         }
     }
-
-
     
     #endregion
 }
