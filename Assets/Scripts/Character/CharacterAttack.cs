962bdb15dea780c2fac14c2169dae5c164d3f32d
ChanYeong-Lee <150050067+ChanYeong-Lee@users.noreply.github.com>
2024-03-05 16:26:09 +0900

    temp commit

--- Assets/Scripts/Character/CharacterAttack.cs
+++ Assets/Scripts/Character/CharacterAttack.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class CharacterAttack : MonoBehaviour
 {
+    public enum AttackState 
+    {
+    }
+
+    private AttackState state;
+    public AttackState State => state;
+
     private Define.AttributeType arrowType;
     public Define.AttributeType ArrowType => arrowType;
 
