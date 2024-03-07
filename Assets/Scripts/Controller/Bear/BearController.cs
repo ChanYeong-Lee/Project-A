7132ec00125d5a8b4303e8b17e6f6be6d3fa8263
ChanYeong-Lee <150050067+ChanYeong-Lee@users.noreply.github.com>
2024-03-07 21:51:36 +0900

    NavMeshTest

--- Assets/Scripts/Controller/Bear/BearController.cs
+++ Assets/Scripts/Controller/Bear/BearController.cs
@@ -1,7 +1,5 @@
-using System;
 using MonsterController;
 using UnityEngine;
-using Random = UnityEngine.Random;
 using State = Define.BearState;
 
 namespace BearController
@@ -15,27 +13,26 @@ namespace BearController
 
         protected bool isChangedState;
 
+        // protected bool isUnderAttack;
         protected float attackCooldown;
-        protected float rushCooldown = 10f;
         protected float vertical;
         protected float horizontal;
         protected int idInt;
 
         protected float distanceToTarget;
         protected float angleToTarget;
+        protected float angleCoefficient = 0.0f;
 
         protected float moreSpeed = 1.0f;
-
-        protected bool canMove;
             
-        public BearState(Creature owner) : base(owner) { }
-        
+        public BearState(Creature owner) : base(owner)
+        {
+        }
+
         public override void Update()
         {
             randTime -= Time.deltaTime;
             attackCooldown -= Time.deltaTime;
-            rushCooldown -= Time.deltaTime;
-            
             bear.angleToTarget = angleToTarget;
             
             if (target != null)
@@ -55,6 +52,7 @@ namespace BearController
             if (bear.state == State.Idle)
                 return;
             
+
             if (Physics.Raycast(bear.Body.transform.position + Vector3.up, Vector3.down, out var hit, Mathf.Infinity))
             {
                 Vector3 normal = hit.normal;
@@ -67,9 +65,9 @@ namespace BearController
                 bear.rootRotation = Quaternion.identity;
 
                 bear.slopeAngle = angle;
-                bear.transform.localRotation = Quaternion.Slerp(bear.transform.localRotation,
-                    Quaternion.Euler(angle, anim.GetFloat("Horizontal") * 180.0f + bear.transform.rotation.eulerAngles.y,
-                        0), Time.fixedDeltaTime);
+                float deltaAngle = angle - bear.transform.rotation.eulerAngles.x;
+                Debug.
+                bear.transform.rotation *= Quaternion.Euler(0.0f, anim.GetFloat("Horizontal") * 90.0f * moreSpeed * Time.fixedDeltaTime, 0);
             }
             else
             {
@@ -77,9 +75,9 @@ namespace BearController
                 bear.rootRotation = Quaternion.identity;
             }
 
-            canMove = CanMove();
+            // isUnderAttack = anim.GetBool("Damaged");
         }
-        
+
         // 랜덤 값 생성
         // 랜덤 수치 값 일부 조정 가능한 함수(최소 시간, 최대 시간, 상태 머신 바뀔 확률)
         protected void RandVariable(float minTime = 1f, float maxTime = 10f, float rateToChange = 0.5f)
@@ -96,29 +94,22 @@ namespace BearController
             if (distanceToTarget < 2)
                 return;
 
-            float angleCoefficient = (Mathf.Abs(angleToTarget) - angle) / 180.0f;
+            angleCoefficient = (Mathf.Abs(angleToTarget) - angle) / 180.0f;
 
             if (angleToTarget <= angle && angleToTarget >= -angle)
-                anim.SetFloat("Horizontal", 0,0.25f, Time.deltaTime);
+                anim.SetFloat("Horizontal", 0.0f);
             else if (angleToTarget > angle)
-                anim.SetFloat("Horizontal", Mathf.Lerp(0, horizontal, angleCoefficient), 0.25f, Time.deltaTime);
+                anim.SetFloat("Horizontal", Mathf.Lerp(0, horizontal, angleCoefficient));
             else if (angleToTarget < -angle)
-                anim.SetFloat("Horizontal", Mathf.Lerp(0,  -horizontal, angleCoefficient), 0.25f, Time.deltaTime);
+                anim.SetFloat("Horizontal", Mathf.Lerp(0,  -horizontal, angleCoefficient));
         }
 
-        protected void MoreSpeed(bool isPower = true)
+        protected void MoreSpeed()
         {
-            if (isPower)
+            if (2.9f < anim.GetFloat("Vertical"))
             {
-                if (2.9f < anim.GetFloat("Vertical"))
-                {
-                    moreSpeed += Time.fixedDeltaTime / 10.0f;
-                    moreSpeed = Mathf.Clamp(moreSpeed, 1.0f, 1.5f);
-                }
-                else
-                {
-                    moreSpeed = Mathf.Lerp(moreSpeed, 1.0f, Time.fixedDeltaTime);
-                }
+                moreSpeed += Time.fixedDeltaTime / 10.0f;
+                moreSpeed = Mathf.Clamp(moreSpeed, 1.0f, 1.5f);
             }
             else
             {
@@ -126,77 +117,5 @@ namespace BearController
             }
             anim.SetFloat("MoreSpeed", moreSpeed);
         }
-
-        protected bool CanMove()
-        {
-            bool col = IsCollision();
-            bool fall = IsFalling();
-            Debug.Log($"Can Move = {canMove}, IsCollision = {col}, IsFalling = {fall}");
-            return !(IsCollision() || IsFalling());
-        }
-        
-        protected bool IsCollision()
-        {
-            Collider[] colliders = new Collider[2];
-            int count = Physics.OverlapBoxNonAlloc(bear.Eyes.position, Vector3.forward + Vector3.up * 0.5f, colliders,bear.transform.rotation, bear.Detection);
-
-            if (count == 0)
-            {
-                anim.SetFloat("Vertical", Mathf.Lerp(anim.GetFloat("Vertical"), vertical, Time.deltaTime));
-                anim.SetFloat("Horizontal", Mathf.Lerp(anim.GetFloat("Horizontal"), horizontal, Time.deltaTime));
-                return false;
-            }
-            else if (count == 1)
-            {
-                var collisionAngle = Vector3.SignedAngle(bear.transform.forward,
-                    colliders[0].transform.position - bear.transform.position, Vector3.up);
-
-                // Debug.Log("patrol turn");
-                switch (collisionAngle)
-                {
-                    case < 90 and > 45:
-                        anim.SetFloat("Horizontal", Mathf.Lerp(anim.GetFloat("Horizontal"), -1, Time.fixedDeltaTime));
-                        anim.SetFloat("Vertical", Mathf.Lerp(anim.GetFloat("Vertical"), 0, Time.fixedDeltaTime * 10));
-                        break;
-                    case < 45 and > 0:
-                        anim.SetFloat("Horizontal", Mathf.Lerp(anim.GetFloat("Horizontal"), -1, Time.fixedDeltaTime));
-                        anim.SetFloat("Vertical", Mathf.Lerp(anim.GetFloat("Vertical"), -1, Time.fixedDeltaTime * 10));
-                        break;
-                    case > -45 and < 0:
-                        anim.SetFloat("Horizontal", Mathf.Lerp(anim.GetFloat("Horizontal"), 1, Time.fixedDeltaTime));
-                        anim.SetFloat("Vertical", Mathf.Lerp(anim.GetFloat("Vertical"), -1, Time.fixedDeltaTime * 10));
-                        break;
-                    case > -90 and < -45:
-                        anim.SetFloat("Horizontal", Mathf.Lerp(anim.GetFloat("Horizontal"), 1, Time.fixedDeltaTime));
-                        anim.SetFloat("Vertical", Mathf.Lerp(anim.GetFloat("Vertical"), 0, Time.fixedDeltaTime * 10));
-                        break;
-                }
-            }
-            else
-            {
-                var collisionAngle = Vector3.SignedAngle(bear.transform.forward,
-                    colliders[0].transform.position - bear.transform.position, Vector3.up);
-                
-                anim.SetFloat("Vertical", -1);
-                anim.SetFloat("Horizontal", collisionAngle > 0 ? 2 : -2);
-            }
-            
-            return true;
-        }
-
-        protected bool IsFalling()
-        {
-            if (Physics.Raycast(bear.transform.position + bear.transform.forward * 5.0f+ Vector3.up, Vector3.down, out var hit,
-                    10, bear.Detection))
-            {
-                return false;
-            }
-
-            // anim.SetFloat("Horizontal", Mathf.Lerp(anim.GetFloat("Horizontal"), 2, Time.fixedDeltaTime));
-            anim.SetFloat("Vertical", Mathf.Lerp(anim.GetFloat("Vertical"), -1, Time.fixedDeltaTime * 10));
-
-            return true;
-        }
     }
-    
 }
